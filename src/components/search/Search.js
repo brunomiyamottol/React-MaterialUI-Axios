2852a4b4e01a14890e4e4ea1d028c36b72f3ddc3
brunomiyamotto <brunomluque@gmail.com>
2018-09-16 16:08:04 -0500

    Removed Dev lines

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -35,7 +35,6 @@ class Search extends Component {
   onAmountChange = (e, index, value) => this.setState({ amount: value });
 
   render() {
-    console.log(this.state.images);
     return (
       <div>
         <TextField
